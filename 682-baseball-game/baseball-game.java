b753a26c19bfc9cda7706d8d3de505026704cdcc
Sayandip Jana <144523053+Sayandip-Jana-1018@users.noreply.github.com>
2026-05-25 20:45:51 +0530

    Time: 1 ms (100.00%) | Memory: 43.4 MB (80.67%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        int[] stack = new int[operations.length];
+        int index = 0; 
+        
+        for (String op : operations) {
+            if (op.equals("+")) {
+                stack[index] = stack[index - 1] + stack[index - 2];
+                index++;
+            } else if (op.equals("D")) {
+                stack[index] = stack[index - 1] * 2;
+                index++;
+            } else if (op.equals("C")) {
+                index--;
+            } else {
+                stack[index] = Integer.parseInt(op);
+                index++;
+            }
+        }
+                int totalSum = 0;
+        for (int i = 0; i < index; i++) {
+            totalSum += stack[i];
+        }
+        
+        return totalSum;
+    }
+}
