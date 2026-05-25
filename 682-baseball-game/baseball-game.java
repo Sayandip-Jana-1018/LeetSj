6e59652b148b85a6c4abca3619e4bb7146976720
Sayandip Jana <144523053+Sayandip-Jana-1018@users.noreply.github.com>
2026-05-25 22:42:27 +0530

    Time: 1 ms (100.00%) | Memory: 43.4 MB (65.30%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,34 +1,27 @@
 class Solution {
-    public int calPoints(String[] str) {
-        int index = 0;
-        int arr[] = new int[str.length];
-        for(int i=0;i < arr.length;i++){
-            if(str[i].equals("C")){
-                arr[index-1] = 0;
+    public int calPoints(String[] operations) {
+        int[] stack = new int[operations.length];
+        int index = 0; 
+        
+        for (String op : operations) {
+            if (op.equals("+")) {
+                stack[index] = stack[index - 1] + stack[index - 2];
+                index++;
+            } else if (op.equals("D")) {
+                stack[index] = stack[index - 1] * 2;
+                index++;
+            } else if (op.equals("C")) {
                 index--;
-                System.out.println(arr[i]);
-            }
-            else if(str[i].equals("D")){
-                arr[index] = arr[index-1]*2;
+            } else {
+                stack[index] = Integer.parseInt(op);
                 index++;
-                System.out.println(arr[i]);
-
-            }
-            else if(str[i].equals("+")){
-                arr[index] = arr[index-1] + arr[index-2];
-                index++;
-                System.out.println(arr[i]);
-            }
-            else {
-                arr[index] += Integer.parseInt(str[i]);
-                index++;
-                System.out.println(arr[i]);
             }
         }
-        int sum = 0;
-        for(int i=0;i < arr.length;i++){
-            sum += arr[i];
+                int totalSum = 0;
+        for (int i = 0; i < index; i++) {
+            totalSum += stack[i];
         }
-        return sum;
+        
+        return totalSum;
     }
 }
