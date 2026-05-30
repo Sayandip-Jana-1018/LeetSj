f89f9dd57d329665de25540c1221a8d57e476659
Sayandip Jana <144523053+Sayandip-Jana-1018@users.noreply.github.com>
2026-05-30 23:31:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (93.52%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,8 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] totalLoot = new int[nums.length + 1];
+        int[] totalLoot = new int[nums.length];
         if(nums.length == 1) return nums[0];
-        if(nums.length == 2) return Math.max(nums[0], nums[1]);
 
         totalLoot[0] = nums[0];
         totalLoot[1] = Math.max(nums[0], nums[1]);
