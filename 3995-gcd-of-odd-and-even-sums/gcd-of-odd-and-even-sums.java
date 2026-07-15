dab8c6c80fb75ec42d3bfd57357b9102695f9b92
Sayandip Jana <144523053+Sayandip-Jana-1018@users.noreply.github.com>
2026-07-15 11:00:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (86.19%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,11 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sumOdd = n*n;
-        int sumEven = n*n + n;
-        return gcd(sumOdd, sumEven);
-    }
-    private int gcd(int sO, int sE) {
-        if(sE == 0) return sO;
-        return gcd(sE, sO % sE);
+        return n;
     }
 }
