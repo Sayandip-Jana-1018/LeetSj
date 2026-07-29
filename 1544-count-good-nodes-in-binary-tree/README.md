d2e4272ad7f53801ec64e38592e0cad24e04c4bf
Sayandip Jana <jsupriti3434@gmail.com>
2026-07-29 17:13:58 +0000

    Add: 1544. Count Good Nodes in Binary Tree (java)

---
+++ 1544-count-good-nodes-in-binary-tree/README.md
@@ -0,0 +1,16 @@
+# 1544. Count Good Nodes in Binary Tree
+
+🟡 **Medium** · `Tree` `Depth-First Search` `Breadth-First Search` `Binary Tree`
+
+## Performance (Measured on LeetCode)
+
+| Metric | Result |
+|--------|--------|
+| Runtime | 2 ms |
+| Memory | 57.2 MB |
+| Language | java |
+
+## Links
+
+- [View Problem on LeetCode](https://leetcode.com/problems/count-good-nodes-in-binary-tree/)
+- [View My Submission](https://leetcode.com/submissions/detail/2085607882/)
---
+++ 1544-count-good-nodes-in-binary-tree/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return countGoodNodes(root, root.val);
+    }
+    private int countGoodNodes(TreeNode root, int maxSoFar) {
+        if(root == null) return 0;
+        int count = 0;
+        if(root.val >= maxSoFar) count = 1;
+
+        int newMax = Math.max(root.val, maxSoFar);
+
+        int leftTree = countGoodNodes(root.left, newMax);
+        int rightTree = countGoodNodes(root.right, newMax);
+
+        return count + leftTree + rightTree;
+    }
+}
